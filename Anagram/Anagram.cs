8f2ec8c8e9cc451f1f882ac68528e72d21274455
PranshuSachan003 <68487765+PranshuSachan003@users.noreply.github.com>
2020-09-07 07:51:52 +0530

    Update Anagram.cs

--- Anagram/Anagram.cs
+++ Anagram/Anagram.cs
@@ -5,13 +5,35 @@ namespace Anagram
 {
     public class AnagramSelector
     {
-        public bool WordPairIsAnagram(string word1, string word2) {
+        public bool WordPairIsAnagram(string word1, string word2) 
+        {
+            int count1[256]={0},count2[256]={0};
+            for(int i=0;i<word1.Length;i++)
+                count1[word1[i]-'a']++;
+            for(int j=0;j<word2.Length;j++)
+                count2[word2[j]-'a']++;
+            if(word1.Length!=word2.Length)
+                return false;
+            else
+            {
+                for(int k=0;k<256;k++)
+                {
+                    if(count1[k]!=count2[k])
+                        return false;
+                }
+            }
             //Insert the correct implementation here
-            return false;
+            return true;
         }
-        public List<string> SelectAnagrams(string word, List<string> candidates) {
-            //Insert the correct implementation here
-            return candidates;
+        public List<string> SelectAnagrams(string word, List<string> candidates) 
+        {
+            List<string> listOfAnagramOfWord = new List<string>();
+            for(int i=0;candidates.Count;i++)
+            {
+                if(WordPairIsAnagram(word,candidates[i])==true)
+                    listOfAnagramOfWord.Add(candidates[i]);
+            }
+            return listOfAnagramOfWord;
         }
     }
 }
