e83c35b913febfb1c58a2338c8fe318c23a4a118
PranshuSachan003 <68487765+PranshuSachan003@users.noreply.github.com>
2020-09-07 08:00:59 +0530

    Update Anagram.cs

--- Anagram/Anagram.cs
+++ Anagram/Anagram.cs
@@ -7,7 +7,10 @@ namespace Anagram
     {
         public bool WordPairIsAnagram(string word1, string word2) 
         {
-            int count1[256]={0},count2[256]={0};
+            int[] count1 = new int[256];
+            int[] count2 = new int[256];
+            count1[256]={0};
+            count2[256]={0};
             for(int i=0;i<word1.Length;i++)
                 count1[word1[i]-'a']++;
             for(int j=0;j<word2.Length;j++)
